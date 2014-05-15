12d7e000641686b37b3aa57af78b74380a16546c
puhrome <phirom.yim@fullsail.edu>
2014-05-15 14:50:13 -0400

    Assignment
    Using variables to assign value in WebStorm in JS

---
+++ Assignment/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Assignment [Values_Variables] Created by Phirom Yim on 5/15/14.
+ */
+
+var yearBorn = 1955; //assign variable
+
+console.log(yearBorn);//print to console, will display whatever you put in parenthesis
+
+console.log("Hello World");//can also print string variable
